d3638d526caf0fbfb70c513fd925160fc8447fbd
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-12-16 21:53:47 +0100

    finished product section

--- components/mainWebsite/Products.jsx
+++ components/mainWebsite/Products.jsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import React from "react";
 import { icon3 } from "@/public/images";
-import { MdAssistWalker } from "react-icons/md";
+import { MdAssistWalker, MdHearing, MdOutlineSportsGolf, MdOutlineDashboardCustomize } from "react-icons/md";
+import { IoEyeOutline, IoAppsOutline } from "react-icons/io5";
+import { SiGoogleassistant } from "react-icons/si";
+import { GiBrain } from "react-icons/gi";
 
 const Products = () => {
   // Product card component
-  const productCard = ({ icon, heading, text }) => {
+  const productCard = ({ icon, heading, text, mt }) => {
     return (
-      <div className="flex flex-col gap-4 lg:gap-[1.88rem]">
+      <div className={`flex flex-col gap-4 lg:gap-[1.88rem] md:mt-${mt}`}>
         <div className="p-[0.69rem] w-[3.125rem] h-[3.125rem] lg:w-[4.375rem] lg:h-[4.375rem] bg-[#E0E0E0] rounded-[0.25rem] flex items-center justify-center shrink-0">
           {icon}
         </div>
@@ -15,7 +18,7 @@ const Products = () => {
           <h1 className="text-lg lg:text-2xl font-bold tracking-wider text-heading">
             {heading}
           </h1>
-          <p className="text-heading capitalize text-sm lg:text-[1rem] tracking-[0.035rem] lg:-tracking-wider w-[21.62rem] lg:w-[17.83rem] opacity-60">
+          <p className="text-heading capitalize text-sm lg:text-[1rem] tracking-[0.035rem] lg:-tracking-wider w-[17rem] lg:w-[17rem] opacity-60 leading-[1.5rem]">
             {text}
           </p>
         </div>
@@ -28,7 +31,7 @@ const Products = () => {
       <div className="md:ml-[2.06rem] ">
         <Image
           src={icon3}
-          className="relative w-[1.5rem] h-[1.5rem] md:w-[4.56rem] md:h-[4.92rem]"
+          className="relative w-[1.5rem] h-[1.5rem] md:w-[12.56rem] md:h-[5.56rem]"
         />
       </div>
       <div className="flex flex-col">
@@ -36,17 +39,53 @@ const Products = () => {
           <h1 className="text-[1.25rem] md:text-[2rem] font-bold tracking-wider md:tracking-[0.08rem] text-heading">
             Our Products
           </h1>
-          <p className="font-medium text-[1rem] lg:text-[1.5rem] w-[21.726rem] lg:w-[34.92rem] tracking-wider text-heading capitalize">
+          <p className="font-medium text-[1rem] lg:text-[1.5rem] w-[17.726rem] lg:w-[34.92rem] tracking-wider text-heading capitalize">
             Discover a Range of Assistive Technologies Tailored for you.
           </p>
         </div>
 
         {/* Product cards */}
-        <div className="mt-[2.75rem] md:mt-[3.7rem]">
+        <div className="mt-[2.75rem] md:mt-[3.7rem] flex flex-col md:flex-row md:flex-wrap px-8 md:px-0 gap-[1.75rem] lg:gap-[3.25rem] justify-center">
           {productCard({
+            // mt: '3rem',
             icon: <MdAssistWalker className="text-primary w-[3.06rem] h-[3.06rem]"/>,
             heading: "Mobility Assistant",
             text: "Move Freely, live independently: Discover Our Mobility Solutions for you",
+          })}
+          {productCard({
+            icon: <IoEyeOutline  className="text-primary w-[3.06rem] h-[3.06rem]"/>,
+            heading: "Vision Devices",
+            text: "See the World Anew: Empowering Lives with Our Vision Devices",
+          })}
+          {productCard({
+            icon: <MdHearing className="text-primary w-[3.06rem] h-[3.06rem]"/>,
+            heading: "Hearing Aids",
+            text: "Sounds of Inclusion: Enhancing Hearing with Our Cutting-Edge Aids",
+          })}
+          {productCard({
+            icon: <SiGoogleassistant className="text-primary w-[3.06rem] h-[3.06rem]"/>,
+            heading: "Speech Devices",
+            text: "Express Yourself with Confidence: Unlock Potential Through Speech Devices",
+          })}
+          {productCard({
+            icon: <GiBrain className="text-primary w-[3.06rem] h-[3.06rem] "/>,
+            heading: "Cognitive Support",
+            text: "Mindful Assistance: Nurturing Cognitive Well-being with Our Supportive Solutions",
+          })}
+          {productCard({
+            icon: <IoAppsOutline className="text-primary w-[3.06rem] h-[3.06rem]"/>,
+            heading: "Assistive Apps",
+            text: "Seamless Living, One App at a Time: Explore Our Assistive Applications",
+          })}
+          {productCard({
+            icon: <MdOutlineSportsGolf className="text-primary w-[3.06rem] h-[3.06rem]"/>,
+            heading: "Adaptive Sports",
+            text: "Play Without Limits: Embrace the Thrill of Adaptive Sports with Our Equipment",
+          })}
+          {productCard({
+            icon: <MdOutlineDashboardCustomize className="text-primary w-[3.06rem] h-[3.06rem]"/>,
+            heading: "Customized Aid",
+            text: "Tailored for You: Unleashing Possibilities with Our Custom Assistive Solutions",
           })}
         </div>
       </div>
