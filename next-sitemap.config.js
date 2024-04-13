893435dc9f31eb5a602b2cc294c57f8171272fbb
Auxtnn <jonnichas@gmail.com>
2024-04-13 14:30:18 -0700

    added sitemap to gitignore

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
-  siteUrl: isProd ? "https://www.assistivteq.com/" : "http://localhost:3000",
-  generateRobotsTXt: isProd,
+  siteUrl: "https://www.assistivteq.com/",
+  generateRobotsTXt: true,
 };
